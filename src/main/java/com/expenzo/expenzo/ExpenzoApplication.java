5deb4d6e0131289d9484df08f306fb49ff69592e
iremyuce <iremyuce3452@gmail.com>
2025-07-20 19:10:21 +0300

    Initial commit for Expenzo project

---
+++ src/main/java/com/expenzo/expenzo/ExpenzoApplication.java
@@ -0,0 +1,13 @@
+package com.expenzo.expenzo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpenzoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExpenzoApplication.class, args);
+    }
+
+}
